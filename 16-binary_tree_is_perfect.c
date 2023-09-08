18b93228b7c1a80dbfd6db88fa38c4752f237bd4
Holberton1990 <ersida9019@gmail.com>
2023-09-08 12:55:33 -0700

    binary_tre

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -27,10 +27,10 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+    size_t left_height, right_height;
+
     if (tree == NULL)
         return (0);
-
-    size_t left_height, right_height;
 
     left_height = binary_tree_height(tree->left);
     right_height = binary_tree_height(tree->right);
