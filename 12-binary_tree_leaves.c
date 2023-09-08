bff24464b07e0d6ab0fc4f801bb1c9fca77bb810
Holberton1990 <ersida9019@gmail.com>
2023-09-08 11:10:34 -0700

    binary

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,20 +9,20 @@
  */
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
-    size_t left_leaves, right_leaves;
+	if (tree == NULL)
+		return (0);
 
-    if (tree == NULL)
-        return (0);
+	size_t left_leaves, right_leaves;
 
-    /* If both left and right children are NULL, it's a leaf */
-    if (tree->left == NULL && tree->right == NULL)
-        return (1);
+	/* If both left and right children are NULL, it's a leaf */
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
 
-    /* Recursively count the leaves in the left and right subtrees */
-    left_leaves = binary_tree_leaves(tree->left);
-    right_leaves = binary_tree_leaves(tree->right);
+	/* Recursively count the leaves in the left and right subtrees */
+	left_leaves = binary_tree_leaves(tree->left);
+	right_leaves = binary_tree_leaves(tree->right);
 
-    /* Return the sum of leaves in left and right subtrees */
-    return (left_leaves + right_leaves);
+	/* Return the sum of leaves in left and right subtrees */
+	return (left_leaves + right_leaves);
 }
 
