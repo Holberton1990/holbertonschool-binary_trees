373a178c68c65f18a87a580b50315445b03b6ea1
Holberton1990 <ersida9019@gmail.com>
2023-09-12 10:41:18 -0700

    binary

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -42,5 +42,6 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (1);
 
 	return binary_tree_is_perfect(tree->left) && 
-		binary_tree_is_perfect(tree->right);
+			binary_tree_is_perfect(tree->right);
 }
+
