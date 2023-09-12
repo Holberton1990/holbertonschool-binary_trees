3aaaad5be123e7d1bf33249f76b503e9bc4d6f1d
Holberton1990 <ersida9019@gmail.com>
2023-09-12 02:24:39 -0700

    binary

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -8,11 +8,10 @@
  * Return: The depth of the node, or 0 if tree is NULL
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
+{
+    if (tree == NULL || tree->parent == NULL)
+        return (0);
 
-
-	if (tree == NULL || tree->parent == NULL)
-		return (0);
-
-	return (1 + binary_tree_depth(tree->parent));
+    return (1 + binary_tree_depth(tree->parent));
 }
 
